81afdce4001803943af8cfbf5b16ef65d97b302d
mmore500 <mmore500.login@gmail.com>
2023-03-12 03:31:58 +0000

    Deploying to binder from @ mmore500/hstrat-evolutionary-inference@b710648c9edbf3cf58113d89f3913a7d6faf673c 🚀

--- pylib/_specify_experimental_treatments_/_apply_selection_pressure_dynamic.py
+++ pylib/_specify_experimental_treatments_/_apply_selection_pressure_dynamic.py
@@ -5,6 +5,6 @@ def apply_selection_pressure_dynamic(params: typing.Dict) -> typing.Dict:
     return {
         **params,
         **{
-            "tournament_size": 8,
+            "tournament_size": 4,
         },
     }
