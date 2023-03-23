a6ec1e51e68e8176bfecd5d0b5b48638007c2d0b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-22 22:16:15 -0700

    Draft, apply new tidy rules

---
+++ tidy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+shopt -s globstar
+
+for file in **/*.tex; do
+  echo "${file}"
+  sed -i '/\\caption{[^%]/s/\\caption{/&%\n/' "${file}"
+  sed -i -E 's/([^[:blank:]])[[:blank:]]+/\1 /g' "${file}"
+  sed -i -E 's/([^%[:space:]])[[:space:]]*\\label\{/\1\n\\label{/g' "${file}"
+  latexindent -w -s -l="$(realpath latexindent.yaml)" -m "${file}"
+done
