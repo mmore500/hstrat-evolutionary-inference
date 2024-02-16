336fa6d67376f99c37af6c3a2ea4c37f13b46c12
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-16 10:31:12 -0500

    Only clone Simulations if missing

--- pipeline-gen3sis/materials/run_simulation.sh
+++ pipeline-gen3sis/materials/run_simulation.sh
@@ -14,7 +14,10 @@ fi
 echo "RNG_SEED ${RNG_SEED}"
 echo "TREATMENT ${TREATMENT}"
 
-git clone https://github.com/project-gen3sis/Simulations.git || :
+if [ ! -d "Simulations" ]; then
+    echo "Simulations directory for gen3sis assets not found, cloning it"
+    git clone https://github.com/project-gen3sis/Simulations.git
+fi
 (cd Simulations; git checkout 8ce6b88a6cd388f4cb4d963bb807ef91633385c6)
 
 singularity run docker://ghcr.io/mmore500/gen3sis@sha256:ec5c901d454e6cbee7e02f5675006ddd38c8c4c94a33027d963779216c9c7b80 Rscript run_simulation.R
