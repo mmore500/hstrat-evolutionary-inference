b5d414ae14a5335b9b43722a241d72dfd77b54ad
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-16 10:43:17 -0500

    Make patience warning more conspicuous

--- pipeline-gen3sis/materials/run_simulation.sh
+++ pipeline-gen3sis/materials/run_simulation.sh
@@ -9,17 +9,17 @@ source ./IMAGE_URI.sh
 ./ensure_assets.sh
 
 if [ ! -d "landscapes" ]; then
-    echo "landscapes directory not found, creating it"
-    echo "this may take a minute!"
+    echo "[INFO] landscapes directory not found, creating it"
+    echo "[INFO] this may take a minute!"
    ./make_landscapes.sh
 fi
 
 echo "RNG_SEED ${RNG_SEED}"
 echo "TREATMENT ${TREATMENT}"
 
-singularity run "${IMAGE_URI} Rscript run_simulation.R
+singularity run "${IMAGE_URI}" Rscript run_simulation.R
 
-singularity run "${IMAGE_URI} alifedata-phyloinformatics-convert toalifedata \
+singularity run "${IMAGE_URI}" alifedata-phyloinformatics-convert toalifedata \
     --input-file "data/treatment=${TREATMENT}+seed=${RNG_SEED}/${TREATMENT}/phy.nex" \
     --input-schema nexus \
     --output-file "data/treatment=${TREATMENT}+seed=${RNG_SEED}/${TREATMENT}/phy.csv"
