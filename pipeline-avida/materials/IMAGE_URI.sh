492a9ef5ecf922e9fe8daf163b196d80888bb1d8
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-17 00:37:57 -0500

    Disable OEE stats for speedup

--- pipeline-avida/materials/IMAGE_URI.sh
+++ pipeline-avida/materials/IMAGE_URI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export IMAGE_URI="docker://ghcr.io/emilydolson/avida-empirical@sha256:0b663e3531a1046329db0039369c65a72e8d2901ddd89ca951f933fd7464abd4"
+export IMAGE_URI="docker://ghcr.io/emilydolson/avida-empirical@sha256:c42ebf8c5bb004180ffd7eeff003cc4c5b9764524d148952ab21b06f987f16fc"
 echo "IMAGE_URI=${IMAGE_URI}"
