0efbc454e9eac2b0f8c706f9c4a502a9e652b89b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-20 23:12:06 -0400

    Cache venv

--- pipeline/snippets/setup_production_dependencies.sh
+++ pipeline/snippets/setup_production_dependencies.sh
@@ -6,7 +6,27 @@ echo "REVISION ${REVISION}"
 module purge || :
 module load GCCcore/10.2.0 Python/3.8.10 || :
 
-VENV_PATH="$(mktemp -d)"
+ln -s "${HOME}/scratch" "/mnt/scratch/${USER}/" || :
+TMPDIR_="${HOME}/scratch/tmp"
+mkdir -p "${TMPDIR_}"
+
+VENV_CACHE_PATH="/${TMPDIR_}/venv-${REVISION}"
+echo "VENV_CACHE_PATH ${VENV_CACHE_PATH}"
+
+
+if test -d "${VENV_CACHE_PATH}" && [[ -n "${REVISION}" ]]
+then
+
+
+echo "venv cache available at ${VENV_CACHE_PATH}"
+VENV_PATH="${VENV_CACHE_PATH}"
+source "${VENV_PATH}/bin/activate"
+
+else
+
+echo "no eligible venv cache available at ${VENV_CACHE_PATH}"
+echo "maybe revision isn't set?"
+VENV_PATH="$(mktemp -d "${TMPDIR_}/XXXXXX")"
 echo "VENV_PATH ${VENV_PATH}"
 
 for try in {0..9}; do
@@ -24,5 +44,10 @@ for try in {0..9}; do
   echo "sleeping ${SLEEP_DURATION} then retrying"
   sleep "${SLEEP_DURATION}"
 done
+
+echo "setting venv cache at ${VENV_CACHE_PATH}"
+ln -s "${VENV_PATH}" "${VENV_CACHE_PATH}" || :
+
+fi
 
 python3 -c "import hstrat; print('hstrat version', hstrat.__version__)"
